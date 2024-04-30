c9a487bb8a5878fc4d033922162c0d2545da7654
Adel Malaeb <166052475+AdelMalaeb@users.noreply.github.com>
2024-04-30 11:07:33 -0400

    Create Test-Scores_Results.py

---
+++ Test-Scores_Results.py
@@ -0,0 +1,30 @@
+def get_scores(model_name:str, model, X_test_data, y_test_data):
+    '''
+    Generate a table of test scores.
+
+    In: 
+        model_name (string):  How you want your model to be named in the output table
+        model:                A fit GridSearchCV object
+        X_test_data:          numpy array of X_test data
+        y_test_data:          numpy array of y_test data
+
+    Out: pandas df of precision, recall, f1, accuracy, and AUC scores for your model
+    '''
+
+    preds = model.best_estimator_.predict(X_test_data)
+
+    auc = roc_auc_score(y_test_data, preds)
+    accuracy = accuracy_score(y_test_data, preds)
+    precision = precision_score(y_test_data, preds)
+    recall = recall_score(y_test_data, preds)
+    f1 = f1_score(y_test_data, preds)
+
+    table = pd.DataFrame({'model': [model_name],
+                          'precision': [precision], 
+                          'recall': [recall],
+                          'f1': [f1],
+                          'accuracy': [accuracy],
+                          'AUC': [auc]
+                         })
+  
+    return table
